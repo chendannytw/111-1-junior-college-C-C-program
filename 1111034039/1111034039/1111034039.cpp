008660fff9e9282990f2ef3a52a075b8332b6ee1
1111034039 <113499421+1111034039@users.noreply.github.com>
2022-09-21 16:21:19 +0800

    Add files via upload

---
+++ 1111034039/1111034039/1111034039.cpp
@@ -0,0 +1,47 @@
+﻿// 1111034039.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+
+int main(void)
+{
+    int integer1 = 0;
+    int integer2 = 0;
+
+    printf("enter first integer:");
+        scanf_s("%d", &integer1);
+
+    printf("enter scend integer:");
+        scanf_s("%d", &integer2);
+
+    int sum = 0;
+    int difference = 0;
+    int product =  0;
+    int quotien = 0;
+    int remainder = 0;
+    sum = integer1 + integer2;
+    difference = integer1 - integer2;
+    product = integer1 * integer2;
+    quotien = integer1 / integer2;
+    remainder = integer1 % integer2;
+    printf("sum is %d\n",sum);
+    printf("difference is %d\n", difference);
+    printf(" productis %d\n", product);
+    printf(" quotien %d\n", quotien);
+    printf("remainder  %d\n", remainder);
+
+
+
+ 
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
