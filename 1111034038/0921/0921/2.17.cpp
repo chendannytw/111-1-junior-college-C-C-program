49924f4c81191e6b805625627dc4a66453f501a4
1111034038 <113499280+1111034038@users.noreply.github.com>
2022-09-21 15:43:24 +0800

    Add files via upload

---
+++ 1111034038/0921/0921/2.17.cpp
@@ -0,0 +1,15 @@
+
+
+int main(void) {
+	int u = 0;
+	int a = 0;
+	int v = 0;
+	int s = 0;
+	int t = 0;
+	printf("¿é¤Ju: ");
+	scanf_s("%d", &u);
+	printf("¿é¤Ja: ");
+	scanf_s("%d", &a);
+	printf("¿é¤Jt: ");
+	scanf_s("%d", &t);
+}
