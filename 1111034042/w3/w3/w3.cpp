2060bc651ed86e89174d4834e4d98983d6fcfb6a
1111034042 <113499144+kenneth0706@users.noreply.github.com>
2022-09-28 16:16:12 +0800

    Add files via upload

---
+++ 1111034042/w3/w3/w3.cpp
@@ -0,0 +1,37 @@
+﻿// w3.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <stdio.h>
+
+int main(void) {
+	printf("輸入三個數字:");
+	int a = 0, b = 0, c = 0;
+	int max = 0;
+	int small = 9999;
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	scanf_s("%d", &c);
+	if (a >= max) {
+		max = a;
+	}
+	else {
+		max = b;
+	}
+	if (c >= max) {
+		max = c;
+	}
+	if (a <= small) {
+		small = a;
+	}
+	else {
+	small = b;
+}
+if (c < small) {
+	small = c;
+}
+	printf("最小%d\n", small);
+    printf("最大%d\n",max);
+}
+
+
+
